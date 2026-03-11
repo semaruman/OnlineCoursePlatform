60b0af22f662bb84918852ec7c7b6a6542d1400c
Semen <semen.ruman@gmail.com>
2026-03-11 16:41:52 +0300

    Добавил метод Get для получения пользователя по имени в UserService

--- OnlineCoursePlatform/Services/UsersService.cs
+++ OnlineCoursePlatform/Services/UsersService.cs
@@ -39,5 +39,40 @@ namespace OnlineCoursePlatform.Services
                 return false;
             }
         }
+
+        /// <summary>
+        /// Получение пользователя из таблицы users
+        /// </summary>
+        /// <param name="fullName">Полное имя пользователя</param>
+        /// <returns>User</returns>
+        public static User Get(string fullName)
+        {
+            using var connection = new MySqlConnection(Constant.ConnectionString);
+            connection.Open();
+
+            string sqlQuery = @"SELECT *
+FROM users
+WHERE full_name = @fullName AND is_active";
+
+            using var command = new MySqlCommand(sqlQuery, connection);
+            
+            command.Parameters.AddWithValue("@fullName", fullName);
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                User user = new User();
+                user.FullName = reader.IsDBNull(0)? null : reader.GetString(0);
+                user.Details = reader.IsDBNull(1) ? null : reader.GetString(1);
+                user.JoinDate = reader.GetDateTime(2);
+                user.Avatar = reader.IsDBNull(3) ? null : reader.GetString(3);
+                user.IsActive = reader.GetBoolean(4);
+
+                return user;
+            }
+
+            return null;
+        }
     }
 }
