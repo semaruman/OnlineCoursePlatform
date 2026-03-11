66977d5db0844b488b32125336237301b804537e
Semen <semen.ruman@gmail.com>
2026-03-11 07:17:13 +0300

    Добавьте файлы проекта.

---
+++ OnlineCoursePlatform/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCoursePlatform
+{
+    public class Constant
+    {
+        public const string ConnectionString = "Server=localhost:3306;Database=test;Uid=root;Pwd=sEmA303033;";
+    }
+}
---
+++ OnlineCoursePlatform/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCoursePlatform.Models
+{
+    public class User
+    {
+        public string FullName { get; set; }
+
+        public string? Details { get; set; }
+
+        public DateTime JoinDate { get; set; } = DateTime.Now;
+
+        public string? Avatar {  get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
