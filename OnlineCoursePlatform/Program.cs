495d8d568a95d37f60c9d8f312e108b0b101597f
Semen <semen.ruman@gmail.com>
2026-03-11 16:10:07 +0300

    Добавил меню в метод Main класса Program

--- OnlineCoursePlatform/Program.cs
+++ OnlineCoursePlatform/Program.cs
@@ -3,6 +3,40 @@ using OnlineCoursePlatform.Services;
 
 public class Program
 {
+    public static void Main(string[] args)
+    {
+        string menuValue;
+        while (true)
+        {
+            Console.WriteLine(@"
+************************************************
+* Добро пожаловать на онлайн платформу Stepik! *
+************************************************
+
+Выберите действие (введите число и нажмите Enter):
+
+1. Зарегистрироваться
+2. Закрыть приложение
+
+************************************************
+");
+            menuValue = Console.ReadLine();
+            if (menuValue == "1")
+            {
+                RegisterUser();
+            }
+            else if (menuValue == "2")
+            {
+                Console.WriteLine("До свидания!");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор. Попробуйте снова.");
+            }
+        }
+    }
+
     public static void RegisterUser()
     {
         Console.WriteLine("Введите имя и фамилию через пробел и нажмите Enter:");
