cbcb44e41f03e277b0b3c382088e566808b28b0d
Semen <semen.ruman@gmail.com>
2026-03-11 16:03:28 +0300

    Добавил метод RegisterUser для регистрации пользователя в Program.cs

--- OnlineCoursePlatform/Program.cs
+++ OnlineCoursePlatform/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using OnlineCoursePlatform.Models;
+using OnlineCoursePlatform.Services;
+
+public class Program
+{
+    public static void RegisterUser()
+    {
+        Console.WriteLine("Введите имя и фамилию через пробел и нажмите Enter:");
+        string fullName = Console.ReadLine();
+
+        User user = new User
+        {
+            FullName = fullName
+        };
+
+        if (UsersService.Add(user))
+        {
+            Console.WriteLine($"Пользователь '{fullName}' успешно добавлен.\n");
+        }
+        else
+        {
+            Console.WriteLine("Произошла ошибка, произведен выход на главную страницу\n");
+        }
+    }
+}
