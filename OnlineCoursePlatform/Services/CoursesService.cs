636f13feebd3a90c3845a23cefbe1c1048187f1d
Semen <semen.ruman@gmail.com>
2026-03-12 13:37:56 +0300

    Добавил метод GetTotalCount() в CourseService для получения общего количества курсов

--- OnlineCoursePlatform/Services/CoursesService.cs
+++ OnlineCoursePlatform/Services/CoursesService.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
 
 namespace OnlineCoursePlatform.Services
 {
     public class CoursesService
     {
+        /// <summary>
+        /// Получение общего количества курсов
+        /// </summary>
+        public static int GetTotalCount()
+        {
+            using var connection = new MySqlConnection(Constant.ConnectionString);
+            connection.Open();
+
+            string sqlQuery = @"SELECT COUNT(id) FROM courses";
+
+            using var command = new MySqlCommand(sqlQuery, connection);
+
+            object res = command.ExecuteScalar();
+
+            return Convert.ToInt32(res);
+        }
     }
 }
