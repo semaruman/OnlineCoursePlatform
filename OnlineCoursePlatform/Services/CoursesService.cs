4443d31dace991384cda8ce32e4258489fba2536
Semen <semen.ruman@gmail.com>
2026-03-12 14:09:27 +0300

    Добавил метод Get() в CourseService для получения курсов по имени пользователя

--- OnlineCoursePlatform/Services/CoursesService.cs
+++ OnlineCoursePlatform/Services/CoursesService.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using OnlineCoursePlatform.Models;
 
 namespace OnlineCoursePlatform.Services
 {
@@ -7,18 +8,40 @@ namespace OnlineCoursePlatform.Services
         /// <summary>
         /// Получение общего количества курсов
         /// </summary>
-        public static int GetTotalCount()
+        public static List<Course> Get(string userFullName)
         {
             using var connection = new MySqlConnection(Constant.ConnectionString);
             connection.Open();
 
-            string sqlQuery = @"SELECT COUNT(id) FROM courses";
+            string sqlQuery = @"
+        SELECT 
+            c.title,
+            c.summary,
+            c.photo
+        FROM users as u
+        JOIN user_courses as uc ON uc.user_id = u.id
+        JOIN courses as c ON uc.course_id = c.id
+        WHERE u.is_active = 1 AND u.full_name = @fullName   
+        ORDER BY uc.last_viewed DESC;";
 
             using var command = new MySqlCommand(sqlQuery, connection);
+            command.Parameters.Add(new MySqlParameter("@fullName", userFullName));
 
-            object res = command.ExecuteScalar();
+            using var reader = command.ExecuteReader();
+            List<Course> coursesList = new List<Course>();
 
-            return Convert.ToInt32(res);
+            while (reader.Read())
+            {
+                Course course = new Course
+                {
+                    Title = reader.GetString(0),
+                    Summary = reader.IsDBNull(1) ? null : reader.GetString(1),
+                    Photo = reader.IsDBNull(2) ? null : reader.GetString(2)
+                };
+                coursesList.Add(course);
+            }
+
+            return coursesList;
         }
     }
 }
