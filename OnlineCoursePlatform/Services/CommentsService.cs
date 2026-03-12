df3c9196e7d143e38be751bebd6a7c3e2797296b
Semen <semen.ruman@gmail.com>
2026-03-12 19:04:52 +0300

    Добавил класс CommentsService для работы с таблицей comments из базы данных и метод Get() для получения списка комментов по id курса

---
+++ OnlineCoursePlatform/Services/CommentsService.cs
@@ -0,0 +1,45 @@
+using MySql.Data.MySqlClient;
+using OnlineCoursePlatform.Models;
+
+namespace OnlineCoursePlatform.Services
+{
+    public class CommentsService
+    {
+        public static List<Comment> Get(int course_idP)
+        {
+            using var connection = new MySqlConnection(Constant.ConnectionString);
+            connection.Open();
+
+            string sqlQuery = @"SELECT c.id, c.text, c.time
+FROM comments AS c
+JOIN steps AS s ON c.step_id = s.id
+JOIN unit_lessons AS ul ON s.lesson_id = ul.lesson_id
+JOIN units AS u ON ul.unit_id = u.id
+JOIN courses AS cr ON u.course_id = cr.id
+WHERE reply_comment_id IS NULL AND cr.id = @course_idP
+ORDER BY c.time DESC;";
+
+            using var command = new MySqlCommand(sqlQuery, connection);
+
+            var courseIdParam = new MySqlParameter("@course_idP", course_idP);
+
+            command.Parameters.Add(courseIdParam);
+
+            using var reader = command.ExecuteReader();
+
+            List<Comment> resList = new List<Comment>();
+            while (reader.Read())
+            {
+                Comment comment = new Comment
+                {
+                    Id = reader.GetInt32("id"),
+                    Text = reader.GetString("text"),
+                    Time = reader.GetDateTime("time")
+                };
+                resList.Add(comment);
+            }
+
+            return resList;
+        }
+    }
+}
