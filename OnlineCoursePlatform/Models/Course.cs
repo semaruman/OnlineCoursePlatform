b28986cdad50d70007a37e455ae36104525fe9fb
Semen <semen.ruman@gmail.com>
2026-03-12 13:26:45 +0300

    Добавил класс Course для описания курса и класс CourseService для работы с таблицей courses из базы данных

---
+++ OnlineCoursePlatform/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCoursePlatform.Models
+{
+    public class Course
+    {
+        public string Title { get; set; }
+
+        public string? Summary { get; set; }
+
+        public string? Photo {  get; set; }
+    }
+}
