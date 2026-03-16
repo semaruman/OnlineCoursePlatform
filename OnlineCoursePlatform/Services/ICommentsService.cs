72cc5afd024fa617211256d325ecd13908334a66
Semen <semen.ruman@gmail.com>
2026-03-16 15:13:50 +0300

    Реализовал интерфейс курсов и комментариев

--- OnlineCoursePlatform/Services/ICommentsService.cs
+++ OnlineCoursePlatform/Services/ICommentsService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnlineCoursePlatform.Models;
 
 namespace OnlineCoursePlatform.Services
 {
     public interface ICommentsService
     {
+        /// <summary>
+        /// Получение всех комментариев к курсу
+        /// </summary>
+        /// <param name="id">id курса</param>
+        /// <returns>Список комментариев</returns>
+        List<Comment> Get(int id);
+
+        /// <summary>
+        /// Удаление комментария пользователя
+        /// </summary>
+        /// <param name="id">id комментария</param>
+        /// <returns>Удалось ли удалить комментарий</returns>
+        bool Delete(int id);
     }
+
 }
