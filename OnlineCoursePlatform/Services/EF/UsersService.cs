0488919914017b3debecd23a6aad50a90b976d3f
Semen <semen.ruman@gmail.com>
2026-03-17 06:03:06 +0300

    Добавил метод Get для получения пользователя по полному имени для EF

--- OnlineCoursePlatform/Services/EF/UsersService.cs
+++ OnlineCoursePlatform/Services/EF/UsersService.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OnlineCoursePlatform.Data;
+using OnlineCoursePlatform.Models;
 
 namespace OnlineCoursePlatform.Services.EF
 {
-    public class UsersService : IUsersService
+    public class UsersService 
     {
+        /// <summary>
+        /// Получение пользователя из таблицы users
+        /// </summary>
+        /// <param name="fullName">Полное имя пользователя</param>
+        /// <returns>User</returns> 
+        public User? Get(string fullName)
+        {
+            using var dbContext = new ApplicationDbContext();
+            var user = dbContext.Users.FirstOrDefault(u => u.full_name == fullName && u.is_active);
+
+            return user;
+        }
     }
 }
